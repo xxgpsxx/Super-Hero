6a79c1ad9978cd61f56d59e7ba1ba20c9542c119
xxgpsxx <gps360.infinityx@gmail.com>
2017-12-08 20:53:54 -0500

    Update Good.java

--- src/mypckg/Good.java
+++ src/mypckg/Good.java
@@ -3,15 +3,12 @@ package mypckg;
 public class Good extends Special
 {
     private String phrase = "";
-    private String heroName = "";
     public Good(String name, String job, int hp, int maxDmg, int defense, String power, Boolean cape, int level, String phrase, String heroName)
     {
-        super(name, job, hp, maxDmg, defense, power, cape, level);
+        super(name, job, hp, maxDmg, defense, power, cape, level, heroName);
         this.phrase = phrase;
-        this.heroName = heroName;
     }
     public void phrase() { System.out.println(phrase); }
-    public String heroName() { return heroName; }
     public void setLevel() { setLevel(super.level() + (int)(Math.random() * 10) + 1); }
     public String toString()
     {
